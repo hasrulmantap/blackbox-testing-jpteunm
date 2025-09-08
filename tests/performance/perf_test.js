c65b9401e9a1d2f9a114dbbad3972c95b88dda8c
hasrulmantap <hasrulmantap@gmail.com>
2025-09-08 16:40:20 +0800

    Update perf_test.js

--- tests/performance/perf_test.js
+++ tests/performance/perf_test.js
@@ -1,1 +1,16 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export let options = {
+    vus: 10,
+    duration: '30s',
+};
+
+export default function () {
+    let res = http.get('https://jpteunm.com');
+    check(res, {
+        'status is 200': (r) => r.status === 200,
+        'response < 500ms': (r) => r.timings.duration < 500,
+    });
+}
 
